2ff8d4df6471da445dc6565ff3d3b92065de9991
mihaigabriel16 <mihai.gabriel1608@gmail.com>
2023-03-02 22:37:25 +0200

    working on cloudbet

--- main.py
+++ main.py
@@ -24,3 +24,27 @@ def getEsportsBetGames():
 
     x = requests.post(url, json = body)
     return x.text
+
+def getCloudBetGames():
+    return 0
+def getCloudBetEvents():
+    url = "https://www.cloudbet.com/sports-api/c/v6/sports/events?sports=league-of-legends&markets=league_of_legends.winner&limit=3000&locale=en"
+    x = requests.get(url)
+    return x.text
+def getCloudBetCompetitions():
+    events_data = getCloudBetEvents()
+    for competition in events_data["sports"]["competitions"]:
+        object = {
+
+            "team1": {
+                "name": None,
+                "odds": None
+            }, 
+            "team2": {
+                "name": None,
+                "odds": None
+            }
+        }
+        url = "https://www.cloudbet.com/sports-api/c/v6/sports/competitions/league-of-legends-international-t673d-lck-cl-spring-season/events?markets=league_of_legends.winner&markets=league_of_legends.map_handicap&locale=en"
+        x = requests.get(url)
+        return x.text
