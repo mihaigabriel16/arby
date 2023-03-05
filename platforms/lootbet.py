75f0ac348b69cf9504bcb2fdb0a53144b88050ae
mihaigabriel16 <mihai.gabriel1608@gmail.com>
2023-03-05 15:13:23 +0200

    fixed lootbet

--- platforms/lootbet.py
+++ platforms/lootbet.py
@@ -34,12 +34,13 @@ def filterGames(data):
         object["key"] = item["tournament"]["Name"]
         object["team1"]["name"] = item["HomeTeamName"]
         object["team2"]["name"] = item["AwayTeamName"]
-        odds = getOdds(item["Id"])
+        odds = getOdds(item["Id"], object["team1"]["name"], object["team2"]["name"])
+        print(odds)
         object["team1"]["odds"] = odds["odds1"]
         object["team2"]["odds"] = odds["odds2"]
         data_array.append(object)
 
-def getOdds(gameId):
+def getOdds(gameId, teamA, teamB):
     url = "https://loot.bet/odds/api/market?match=" + gameId + "&weight=negative"
     x = requests.get(url)
     object = {
@@ -47,14 +48,16 @@ def getOdds(gameId):
         "odds2": None
     }
     data = json.loads(x.text)
-    try:
-        for item in data:
-            if item["MatchId"] == gameId:
-                if item["MarketTemplateId"] == 141:
-                    for odd in item["odds"]:
-                        object["odds1"] = odd[0]["Value"]
-                        object["odds2"] = odd[1]["Value"]
-        return object
-    except KeyError:
-        pass
+    for item in data:
+        if item["MatchId"] == gameId:
+            if item["MarketTemplateId"] == 141:
+                if len(item["odds"]) > 0:
+                    object["odds1"] = item["odds"][0]["Value"]
+                    object["odds2"] = item["odds"][1]["Value"]
+                    #for odd in item["odds"]:
+                    #    if odd["Name"] == teamA:
+                    #        object["odds1"] = odd["Value"]
+                    #    else:
+                    #        object["odds2"] = odd["Value"]
+    return object
     
